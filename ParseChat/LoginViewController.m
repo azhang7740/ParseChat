120166ed2f2a5a796faef49090a593955e21743d
Angelina Zhang <azhang7740@gmail.com>
2022-06-27 09:09:29 -0700

    ParseChat UI elements

---
+++ ParseChat/LoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ParseChat
+//
+//  Created by Angelina Zhang on 6/27/22.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
